94ff97a317a55c87822de615dd5fa2f5537b2acd
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 <hugovarellaa@gmail.com>
2022-07-19 17:19:44 -0300

    Estrutura de comentarios

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/Hugovarellaa.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.CommentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Hugo Varella</strong>
+              <time title="11 de Maio às 08:00h" dateTime="2022-05-11 08:00:00">
+                Cerca de 2h atrás
+              </time>
+            </div>
+            <button title="Deleta comentário">
+              <Trash />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <ThumbsUp />
+          Aplaudir <span>20</span>
+        </footer>
+      </div>
+    </div>
+  );
+}
