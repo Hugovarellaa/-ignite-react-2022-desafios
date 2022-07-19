7e5a0263bd012eefc7c02160369d0ebd967d0c06
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 <hugovarellaa@gmail.com>
2022-07-19 15:01:47 -0300

    Estrutura Do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 import "./styles/global.css";
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>Post</main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <div className={styles.sidebarContainer}>
+      <h1>Sidebar</h1>
+    </div>
+  );
+}
