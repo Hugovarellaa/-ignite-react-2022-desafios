deb226dc36cd62d71f9b8e3e781570a2dba375f4
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-07-28 14:50:05 -0300

    Delete ignite-todo-list directory

--- ignite-todo-list/src/components/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-import logoImg from "../../assets/logo.svg";
-import styles from "./Header.module.css";
-export function Header() {
-  return (
-    <header className={styles.header}>
-      <img src={logoImg} alt="Foguete azul com o nome Todo" />
-    </header>
-  );
-}
